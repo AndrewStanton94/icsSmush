ffb3797c71ceea54eb504a61e361267926a0524e
Andrew Stanton <mail@andrewstanton.tech>
2023-09-15 00:13:37 +0100

    Trimmed down the summary to station codes

--- prepare.sh
+++ prepare.sh
@@ -7,6 +7,9 @@ if [ ! -f "${scratchDir}" ]; then
 	mv ~/Downloads/*.ics "${scratchDir}"
 	cat ./before.txt > "${currentDate}.ics"
 	sed -n '/^BEGIN:VEVENT/,/^END:VEVENT/p' "${scratchDir}"/*.ics >> "${currentDate}.ics"
+  # Extract the station codes from the summary
+  # Using the first capture group like a lookbehind
+  sed -i 's/\(SUMMARY:\).*Journey Details: .*(\(.*\)) to .*(\(.*\))/\1\2=>\3/g' "${currentDate}.ics"
 	cat ./after.txt >> "${currentDate}.ics"
 fi
 
